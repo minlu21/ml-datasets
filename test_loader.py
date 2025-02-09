7b938e58b4408ddb2d5ebbbd1ca80cc10543305c
Min Lu <minlu@lawn-128-61-62-238.lawn.gatech.edu>
2025-02-09 16:56:15 -0500

    added dataset files to gitignore and imagenet10K dataloader

---
+++ test_loader.py
@@ -0,0 +1,26 @@
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+
+from loaders import imagenet as imgnet
+
+def show_tensor_img(img, label):
+    demo_array = np.moveaxis(img.numpy() * 255, 0, -1)
+    Image._show((Image.fromarray(demo_array.astype(np.uint8))))
+    print(f"Label: {label}")
+
+
+def test_imagenet():
+    imagenet = imgnet.ImageNet10K(transform=transforms.Resize((224, 224)))
+    print(f"{len(imagenet.split_dataset()[0])}")
+    train_dataloader, _, _ = imagenet.get_dataloaders()
+    train_features, train_labels = next(iter(train_dataloader))
+    print(f"Feature batch shape: {train_features.size()}")
+    print(f"Label batch shape: {train_labels.size()}")
+    img = train_features[0].squeeze()
+    label = train_labels[0]
+    show_tensor_img(img, label)
+    
+
+if __name__ == "__main__":
+    test_imagenet()
